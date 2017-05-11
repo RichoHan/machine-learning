bd1ae1dad6e812dd904fb5169faccd07a086fdcd
richo_han <Richo_Han@asus.com>
2017-05-11 18:35:09 +0800

    Add data observation in notebook.

--- session-1-pm2.5-prediction/run.py
+++ session-1-pm2.5-prediction/run.py
@@ -1,5 +1,17 @@
+#!/usr/bin/python
+# -*- coding: big5 -*-
+
 """Session 1 Linear Regression"""
 import pandas as pd
+import matplotlib.dates as mdates
+import numpy as np
+from datetime import datetime
 
 if __name__ == "__main__":
-    DATA = pd.read_csv('./data/train.csv', encoding="big5")
+    data = pd.read_csv('./data/train.csv', encoding="big5")
+    pm_25_date = data[data['測項'] == 'PM2.5'].loc[:, '日期']
+    dates = [datetime.strptime(date, '%Y/%m/%d') for date in pm_25_date.values]
+
+    pm_25 = data[data['測項'] == 'PM2.5'].loc[:, '0':'23'].apply(pd.to_numeric)
+    daily_pm_25 = pm_25.mean(axis=1)
+    print(pm_25.describe())
