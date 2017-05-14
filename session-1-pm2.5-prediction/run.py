41a8ac9da7988a757a061afa29c6b7c9695e6a7c
RichoHan <chenghsien.richo@gmail.com>
2017-05-14 23:34:03 +0800

    Update notebook.

--- session-1-pm2.5-prediction/run.py
+++ session-1-pm2.5-prediction/run.py
@@ -3,22 +3,18 @@
 
 """Session 1 Linear Regression"""
 import pandas as pd
-import matplotlib.dates as mdates
-import numpy as np
-from datetime import datetime
 
 if __name__ == "__main__":
     data = pd.read_csv('./data/train.csv', encoding="big5")
-    pm_25_date = data[data['測項'] == 'PM2.5'].loc[:, '日期']
-    dates = [datetime.strptime(date, '%Y/%m/%d') for date in pm_25_date.values]
-
-    pm_25 = data[data['測項'] == 'PM2.5'].loc[:, '0':'23'].apply(pd.to_numeric)
-
     features = data.groupby('測項')
-    for (feature, group) in features:
-        print(feature)
-        values = group.loc[:, '0':'23']
-        if feature == 'RAINFALL':
-            values = values.replace('NR', '0')
-        means = values.apply(pd.to_numeric).describe().loc['mean', :]
-        print(means.describe())
+    pm_25 = data[data['測項'] == 'PM2.5'].loc[:, '0':'23'].apply(pd.to_numeric)
+    print(pm_25.describe())
+    # y = pm_25.describe().loc['mean', :]
+
+    # for (feature, group) in features:
+    #     print(feature)
+    #     values = group.loc[:, '0':'23']
+    #     if feature == 'RAINFALL':
+    #         values = values.replace('NR', '0')
+    #     means = values.apply(pd.to_numeric).describe().loc['mean', :]
+    #     print(means.describe())
