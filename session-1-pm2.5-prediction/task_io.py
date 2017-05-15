e58cbe8de655813496992cb761dbf121b2a5ed4a
Richo <chenghsien.richo@gmail.com>
2017-05-15 18:43:53 +0800

    Update execution python file and ignore files.

---
+++ session-1-pm2.5-prediction/task_io.py
@@ -0,0 +1,20 @@
+from abc import ABCMeta, abstractmethod
+
+import pandas as pd
+
+
+class TaskIO(metaclass=ABCMeta):
+    def __init__(self, train, test, result):
+        self.train = train
+        self.test = test
+        self.result = result
+
+    def import_training_data(self):
+        return pd.read_csv(self.train, encoding="big5")
+
+    def import_testing_data(self):
+        return pd.read_csv(self.test, encoding="big5")
+
+    @abstractmethod
+    def export_prediction(self, data):
+        pass
