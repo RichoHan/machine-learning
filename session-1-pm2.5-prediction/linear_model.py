da80aa2cb59a4f3eaa83802722461d74ddaf63f4
Richo <chenghsien.richo@gmail.com>
2017-05-17 18:19:33 +0800

    Update scripts.

--- session-1-pm2.5-prediction/linear_model.py
+++ session-1-pm2.5-prediction/linear_model.py
@@ -2,21 +2,100 @@
 from sklearn.linear_model import LinearRegression
 
 
-class LinearRegression(LinearRegression):
+class LinearRegression2(LinearRegression):
     def __init__(self):
         super().__init__()
 
-    def gradient_descent(eta, x, y, end_point=0.0001, max_iter=10000):
-        """Gradient descent is used to calculate the best fitting function for the model.
+    def _gradient_descent(self, eta, x, y, end_point=0.0001, max_iter=10000):
+        """Gradient descent is used to calculate the best fitting function: y = b + wx
 
-        Args:
-            eta (float): Learning rate.
-            x (ndarray): The training input.
-            y (ndarray): The training output.
-            end_point (float): Convergence criteria.
-            max_iter (int): Maximum number of iteration.
+        Parameters
+        ----------
+        eta (float): Learning rate.
 
-        Returns:
-            tuple: A pair of intercept(=theta0) and slope(=theta1).
+        x (ndarray): Training data.
+
+        y (ndarray): Target value.
+
+        end_point (float): Convergence criteria.
+
+        max_iter (int): Maximum number of iteration.
+
+        Returns
+        -------
+        tuple: A pair of intercept(=bias) and slope(=weight).
         """
-        pass
+        # Initialization
+        size = x.shape[0]
+        converged = False
+        iteration = 0
+        b, w = 0, 0
+
+        # Goodness of function
+        loss = sum([(b + w * x[i] - y[i])**2 for i in range(size)])
+        print("x.shape: {0}".format(x.shape))
+        print("size: {0}".format(size))
+        print("loss: {0}".format(loss))
+
+        # Converge iteration
+        while not converged and iteration < max_iter:
+            # For each training sample, compute the gradient (d/d_theta j(theta))
+            grad_0 = (1.0 / size) * sum([(b + (w * x[i]) - y[i]) for i in range(size)]) 
+            grad_1 = (1.0 / size) * sum([(b + (w * x[i]) - y[i]) * x[i] for i in range(size)])
+
+            # Update bias and weight with eta
+            temp_b = b - eta * grad_0
+            temp_w = w - eta * grad_1
+            b = temp_b
+            w = temp_w
+
+            # Compute the error again
+            error = sum([(b + w * x[i] - y[i])**2 for i in range(size)])
+
+            if abs(error - loss) <= end_point:
+                converged = True
+
+            loss = error
+            iteration = iteration + 1
+        return b, w
+
+    def fit(self, X, y):
+        """Compute the fit function for the model.
+
+        Parameters
+        ----------
+        X (ndarray): Training data.
+
+        y (ndarray): Target value.
+
+        Returns
+        -------
+        self : returns an instance of self.
+        """
+
+        eta = 0.01  # learning rate
+        end_point = 0.01  # convergence criteria
+
+        # call gredient decent, and get intercept(=bias) and slope(=weight)
+        bias, weight = self._gradient_descent(eta, X, y, end_point, max_iter=1000)
+        print('bias = {0}, weight = {1}'.format(bias, weight))
+
+        super().fit(X, y)
+        return self
+
+    def predict(self, X):
+        """Predict using the linear model
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = (n_samples, n_features)
+            Samples.
+
+        Returns
+        -------
+        C : array, shape = (n_samples,)
+            Returns predicted values.
+        """
+        # return self._decision_function(X)
+        y = 1 + 5 * X
+        return y
--- session-1-pm2.5-prediction/run.py
+++ session-1-pm2.5-prediction/run.py
@@ -43,45 +43,43 @@ if __name__ == "__main__":
     outcome = pm_25
     for hour in feature:
         x = x.append(feature[hour].map(lambda elem: elem), ignore_index=True)
-
-    for hour in outcome:
         y = y.append(outcome[hour].map(lambda elem: elem), ignore_index=True)
 
     # ===== Fitting linear model =====
-    from linear_model import LinearRegression
-    model = LinearRegression()
+    from linear_model import LinearRegression2
+    model = LinearRegression2()
 
     model.fit(x[:, np.newaxis], y)
 
-    xfit = np.linspace(0, feature.max().max(), 1000)
-    yfit = model.predict(xfit[:, np.newaxis])
+    # xfit = np.linspace(0, feature.max().max(), 1000)
+    # yfit = model.predict(xfit[:, np.newaxis])
 
     # ===== Prediction =====
-    pm_10 = testing_data[testing_data[1] == 'PM10'].iloc[:, 2:11].apply(pd.to_numeric)
-    pm_25_answer = testing_data[testing_data[1] == 'PM2.5'].iloc[:, 11].apply(pd.to_numeric)
-    prediction_from_pm_10 = pm_10.apply(
-        lambda row: row[10],
-        axis=1
-    )
-    prediction_from_pm_10 = model.predict(prediction_from_pm_10[:, np.newaxis]).astype('int')
+    # pm_10 = testing_data[testing_data[1] == 'PM10'].iloc[:, 2:11].apply(pd.to_numeric)
+    # pm_25_answer = testing_data[testing_data[1] == 'PM2.5'].iloc[:, 11].apply(pd.to_numeric)
+    # prediction_from_pm_10 = pm_10.apply(
+    #     lambda row: row[10],
+    #     axis=1
+    # )
+    # prediction_from_pm_10 = model.predict(prediction_from_pm_10[:, np.newaxis]).astype('int')
 
-    print("\n===== prediction_from_pm_10 =====")
-    print(prediction_from_pm_10)
-    print("\n===== pm_2.5_answer =====")
-    print(pm_25_answer.values)
-    print("\n===== RMSE =====")
-    print(rmse(prediction_from_pm_10, pm_25_answer))
+    # print("\n===== prediction_from_pm_10 =====")
+    # print(prediction_from_pm_10)
+    # print("\n===== pm_2.5_answer =====")
+    # print(pm_25_answer.values)
+    # print("\n===== RMSE =====")
+    # print(rmse(prediction_from_pm_10, pm_25_answer))
 
     # ===== Exporting prediction result =====
-    ids = testing_data[testing_data[1] == 'PM10'].iloc[:, 0]
-    result = pd.concat(
-        [
-            ids.to_frame('id').reset_index(drop=True),
-            pd.DataFrame.from_items([('value', prediction_from_pm_10)]).reset_index(drop=True)
-        ],
-        axis=1,
-        ignore_index=True
-    )
-    result.columns = ['id', 'value']
+    # ids = testing_data[testing_data[1] == 'PM10'].iloc[:, 0]
+    # result = pd.concat(
+    #     [
+    #         ids.to_frame('id').reset_index(drop=True),
+    #         pd.DataFrame.from_items([('value', prediction_from_pm_10)]).reset_index(drop=True)
+    #     ],
+    #     axis=1,
+    #     ignore_index=True
+    # )
+    # result.columns = ['id', 'value']
 
-    task_io.export_prediction(result)
+    # task_io.export_prediction(result)
