dd8c9e1184896665dcef84e14e929dc9aca581f1
richo_han <Richo_Han@asus.com>
2017-07-07 13:04:27 +0800

    Update scripts.

--- code/linear_model.py
+++ code/linear_model.py
@@ -1,10 +1,21 @@
 "Linear model for assignment 2."
+import numpy as np
+
 from sklearn import linear_model
 
 
 class LogisticRegression(linear_model.LogisticRegression):
     def __init__(self):
         super().__init__()
+
+    def _sigmoid(self, X):
+        return 1.0 / (1.0 + np.exp(-X))
+
+    def _cost(self, theta, X, y):
+        pass
+
+    def _gradient(self, theta, X, y):
+        pass
 
     def fit(self, X, y):
         """Compute the fit function for the model.
@@ -19,6 +30,7 @@ class LogisticRegression(linear_model.LogisticRegression):
         -------
         self : returns an instance of self.
         """
+        self._cost(0, X, y)
         super().fit(X, y)
 
     def predict(self, X):
