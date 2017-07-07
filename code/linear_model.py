367f3bc63554c6e7114936fc6c04d52d449c2abe
richo_han <Richo_Han@asus.com>
2017-07-07 15:02:55 +0800

    Update scripts.

--- code/linear_model.py
+++ code/linear_model.py
@@ -8,14 +8,33 @@ class LogisticRegression(linear_model.LogisticRegression):
     def __init__(self):
         super().__init__()
 
-    def _sigmoid(self, X):
-        return 1.0 / (1.0 + np.exp(-X))
+    def _sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
 
     def _cost(self, theta, X, y):
-        pass
+        # Prepare to do matrix multiplication
+        theta = np.matrix(theta)
+        X = np.matrix(X)
+        y = np.matrix(y)
+
+        # Calculate -lnL(w, b)
+        p1 = np.multiply(-y, np.log(self._sigmoid(X * theta.T)))
+        p2 = np.multiply((1 - y), np.log(1 - self._sigmoid(X * theta.T)))
+        return np.sum(p1 - p2) / (len(X))
 
     def _gradient(self, theta, X, y):
-        pass
+        # Prepare to do matrix multiplication
+        theta = np.matrix(theta)
+        X = np.matrix(X)
+        y = np.matrix(y)
+        n_params = theta.shape[1]
+
+        error = self._sigmoid(X * theta.T) - y
+        vfunc = np.vectorize(lambda i: np.sum(np.multiply(error, X[:, i])) / len(X))
+        indice = np.array(range(n_params))
+        grad = vfunc(indice)
+
+        return grad
 
     def fit(self, X, y):
         """Compute the fit function for the model.
@@ -30,7 +49,10 @@ class LogisticRegression(linear_model.LogisticRegression):
         -------
         self : returns an instance of self.
         """
-        self._cost(0, X, y)
+        n_params = X.shape[1]
+        theta = np.zeros(n_params)
+        print(self._cost(theta, X, y))
+        print(self._gradient(theta, X, y))
         super().fit(X, y)
 
     def predict(self, X):
