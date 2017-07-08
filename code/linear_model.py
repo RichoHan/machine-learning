85b0142532b6a26cc254b4fc4882afaf95f7ada4
RichoHan <chenghsien.richo@gmail.com>
2017-07-08 21:18:49 +0800

    Update scripts.

--- code/linear_model.py
+++ code/linear_model.py
@@ -13,7 +13,9 @@ class LogisticRegression(linear_model.LogisticRegression):
 
     def _cost(self, theta, X, y):
         p1 = self._sigmoid(np.dot(X, theta))
-        log_l = (-y) * np.log(p1) - (1 - y) * np.log(1 - p1)  # Calculate -lnL(w, b)
+
+        # Calculate -lnL(w, b), add 1e-10 to make sure denominator is no zero.
+        log_l = (-y) * np.log(p1) - (1 - y) * np.log(1 - p1 + 1e-10)
 
         return log_l.mean()
 
@@ -25,8 +27,14 @@ class LogisticRegression(linear_model.LogisticRegression):
 
     def _gradient_descent(self, theta, X, y):
         import scipy.optimize as opt
-        result = opt.fmin_bfgs(f=self._cost, x0=theta, fprime=self._gradient, args=(X, y))
-        return result
+        result = opt.minimize(
+            fun=self._cost,
+            x0=theta,
+            args=(X, y),
+            method='BFGS',
+            jac=self._gradient
+        )
+        return result['x']
 
     def fit(self, X, y):
         """Compute the fit function for the model.
@@ -42,7 +50,7 @@ class LogisticRegression(linear_model.LogisticRegression):
         self : returns an instance of self.
         """
         n_params = X.shape[1]
-        self.theta = np.zeros(n_params)
+        self.theta = np.zeros(n_params, dtype=np.float128)
         self.theta = self._gradient_descent(self.theta, X, y)
 
     def predict(self, X):
